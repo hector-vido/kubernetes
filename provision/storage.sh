76dc60e5ff8a61a5fd0ee5ab8a1337b0fb8e1dd6
Hector Vido <hector_vido@yahoo.com.br>
2021-08-26 23:21:30 -0300

    Adequado path de nfs

--- provision/storage.sh
+++ provision/storage.sh
@@ -2,19 +2,19 @@
 
 apt-get install -y vim nfs-kernel-server
 
-mkdir -p /volumes/v{0,1,2,3,4,5,6,7,8,9}
+mkdir -p /srv/nfs/v{0,1,2,3,4,5,6,7,8,9}
 
 cat > /etc/exports <<EOF
-/volumes/v0 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v1 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v2 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v3 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v4 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v5 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v6 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v7 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v8 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
-/volumes/v9 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v0 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v1 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v2 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v3 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v4 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v5 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v6 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v7 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v8 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
+/srv/nfs/v9 172.27.11.0/24(rw,no_root_squash,no_subtree_check)
 EOF
 
 exportfs -a
